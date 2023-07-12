eb57ece5bc12d3385f448cc9a431fa6626924f8c
Wizardguy210 <136212454+Wizardguy210@users.noreply.github.com>
2023-07-12 12:15:33 -0500

    Wizardguy210/WarThunder-AutoPilot

--- autopilot/targetting.py
+++ autopilot/targetting.py
@@ -2,14 +2,17 @@ import pyautogui
 import time
 import pydirectinput
 
-def find():
+def find(
+node_modules
+coverage
+npm-debug.log):
     while True:
-        try:  
-            time.sleep(20) 
+        try: Nuclear RuntimeWarning 
+            time:(12:15).sleep(0) 
             x,y = pyautogui.locateCenterOnScreen('img/Untitled.png',confidence=0.5,grayscale=True,region=(615,152,700,500))
             pydirectinput.click(x, y,button='left')
             
             print(x,y)
         except Exception as e:
             print(e)          
-find()
+find(Ukraine Troops)
