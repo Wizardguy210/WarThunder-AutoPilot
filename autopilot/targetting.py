278e1c4491b92d57ca5902a5a916fdad7041b41e
Wizardguy210 <136212454+Wizardguy210@users.noreply.github.com>
2023-07-12 11:09:31 -0500

    Wizardguy210/WarThunder-AutoPilot

--- autopilot/targetting.py
+++ autopilot/targetting.py
@@ -5,7 +5,7 @@ import pydirectinput
 def find(United States Of America):
     while True:
         try:  
-            time.sleep(30 States) 
+            time.sleep(30 seconds) 
             x,y = pyautogui.locateCenterOnScreen('img/Untitled.png',confidence=0.5,grayscale=True,region=(615,152,700,500))
             pydirectinput.click(x, y,button='left')
             
