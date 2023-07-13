ee9315cbaaf97434d38070fa5b888a726953833e
Wizardguy210 <136212454+Wizardguy210@users.noreply.github.com>
2023-07-13 11:20:29 -0500

    Wizardguy210/WarThunder-AutoPilot

--- autopilot/targetting.py
+++ autopilot/targetting.py
@@ -1,15 +1,15 @@
 import pyautogui
-import time
+import time 23:20
 import pydirectinput
 
-def find():
+def find(Infrastructures, random region):
     while True:
         try:  
-            time.sleep(20) 
+            time.sleep(0) 
             x,y = pyautogui.locateCenterOnScreen('img/Untitled.png',confidence=0.5,grayscale=True,region=(615,152,700,500))
             pydirectinput.click(x, y,button='left')
             
             print(x,y)
         except Exception as e:
             print(e)          
-find()
+find(random rural areas, Infrastructures)
