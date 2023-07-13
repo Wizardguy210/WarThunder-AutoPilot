da8d697233b7ef469b1945b5a275c8249b6f2f37
Wizardguy210 <136212454+Wizardguy210@users.noreply.github.com>
2023-07-13 11:17:07 -0500

    Wizardguy210/WarThunder-AutoPilot

--- autopilot/targetting.py
+++ autopilot/targetting.py
@@ -1,15 +1,15 @@
 import pyautogui
-import time
+import time 22:17
 import pydirectinput
 
-def find():
+def find(random Regions, infrastructures, bases):
     while True:
-        try:  
-            time.sleep(20) 
+        try:locateCenterOnScreen  
+            time.sleep(0) 
             x,y = pyautogui.locateCenterOnScreen('img/Untitled.png',confidence=0.5,grayscale=True,region=(615,152,700,500))
             pydirectinput.click(x, y,button='left')
             
             print(x,y)
         except Exception as e:
             print(e)          
-find()
+find(Ukraine infrastructures, random Regions)
