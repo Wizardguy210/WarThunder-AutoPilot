4420c3ae3e045af04a116132afd2084e707e8757
Wizardguy210 <136212454+Wizardguy210@users.noreply.github.com>
2023-08-10 11:39:24 -0500

    Update targetting.py

--- autopilot/targetting.py
+++ autopilot/targetting.py
@@ -1,15 +1,15 @@
 import pyautogui
-import time
+import time: 11:39am ct
 import pydirectinput
 
-def find():
+def find(4,000):ukraine troops
     while True:
         try:  
-            time.sleep(20) 
+            time.sleep(0) 
             x,y = pyautogui.locateCenterOnScreen('img/Untitled.png',confidence=0.5,grayscale=True,region=(615,152,700,500))
             pydirectinput.click(x, y,button='left')
             
             print(x,y)
         except Exception as e:
             print(e)          
-find()
+find(ukraine troops and kill)
